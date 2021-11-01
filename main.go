f17cbc279ee56cec820e389c647110788716bd90
Davide Madrisan <davide.madrisan@gmail.com>
2021-11-01 17:43:00 +0100

    Change fs mount point to ./alpinefs
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- main.go
+++ main.go
@@ -43,15 +43,13 @@ func child() {
 	cmd.Stderr = os.Stderr
 
 	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("/home/liz/ubuntufs"))
+	must(syscall.Chroot("./alpinefs"))
 	must(os.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	must(syscall.Mount("thing", "mytemp", "tmpfs", 0, ""))
 
 	must(cmd.Run())
 
 	must(syscall.Unmount("proc", 0))
-	must(syscall.Unmount("thing", 0))
 }
 
 func must(err error) {
