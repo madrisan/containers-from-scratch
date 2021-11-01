98f35d66a893a36345af30efdb59fda69b17c6a7
Davide Madrisan <davide.madrisan@gmail.com>
2021-11-01 19:43:23 +0100

    Support rootless containers
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # containers-from-scratch
-Writing a container in a few lines of Go code, as seen at [DockerCon 2017](https://www.youtube.com/watch?v=MHv6cWjvQjM&t=1316s) and on [O'Reilly Safari](https://www.safaribooksonline.com/library/view/how-to-containerize/9781491982310/)
 
-You need root permissions for this version to work. Or you can adapt it to be a rootless container by as shown in [these slides](https://speakerdeck.com/lizrice/rootless-containers-from-scratch). 
+Writing a container in a few lines of Go code, as seen at [DockerCon 2017](https://www.youtube.com/watch?v=MHv6cWjvQjM&t=1316s) and on [O'Reilly Safari](https://www.safaribooksonline.com/library/view/how-to-containerize/9781491982310/)
 
 Note that the Go code uses some syscall definitions that are only available when building with GOOS=linux.
--- main.go
+++ main.go
@@ -20,14 +20,49 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as user %d in process %d\n",
+		os.Args[2:],
+		os.Geteuid(),
+		os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	os.Clearenv()
+	cmd.Env = []string{
+		"PATH=/bin:/usr/bin",
+		"PS1=[\\u@\\h \\w]\\$ ",
+	}
+
+	// set NameSpaces isolation
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS | // Unix Timesharing System (hostname)
+			syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWNS | // mount points
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWUSER,
+		// map the files to user root and allow running a rootless
+		// container (with no mapping we'll end with the error message:
+		// panic: fork/exec /proc/self/exe: operation not permitted)
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
+		// do not share the mount points to the host
+		// (will not be shown in the mount command output but
+		// you can still see it in the /proc filesystem)
 		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
@@ -35,7 +70,10 @@ func run() {
 }
 
 func child() {
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as user %d in process %d\n",
+		os.Args[2:],
+		os.Geteuid(),
+		os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
